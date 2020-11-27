7b0472ce1b2c94c48837d88cc64e1ef4fe418475
하창진 <hachangjin@hachangjin-ui-MacBookPro.local>
2020-11-27 17:55:49 +0900

    You can use Tracking tab and re-designed with Fragment

--- app/src/main/java/kr/ac/jbnu/se/ssad_group6/fragment/Fragment_Map.java
+++ app/src/main/java/kr/ac/jbnu/se/ssad_group6/fragment/Fragment_Map.java
@@ -2,19 +2,128 @@ package kr.ac.jbnu.se.ssad_group6.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.util.FusedLocationSource;
+
 import kr.ac.jbnu.se.ssad_group6.R;
 
-public class Fragment_Map extends Fragment {
+public class Fragment_Map extends Fragment implements OnMapReadyCallback {
+    private MapView mapView;
+    private FusedLocationSource locationSource;
+    private NaverMap naverMap;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FragmentManager manager;
+
+    public Fragment_Map(){
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment__map, container, false);
+        View view = inflater.inflate(R.layout.fragment__map,
+                container, false);
+
+        mapView = view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+        return view;
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart()
+    {
+        String addr;
+
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onDestroyView()
+    {
+        super.onDestroyView();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory()
+    {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    public static Fragment_Map newInstance(){
+        Fragment_Map map = new Fragment_Map();
+
+        return map;
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        if(locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)){
+            if (!locationSource.isActivated()){
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            }
+
+            return;
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
 }
